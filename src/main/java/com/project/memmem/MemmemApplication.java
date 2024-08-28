b9e1b70d8418bc9bad8c9503a49bc69c4ee1da3a
jyj <hikenny96@naver.com>
2024-08-28 14:37:34 +0900

    feat: first commit

---
+++ src/main/java/com/project/memmem/MemmemApplication.java
@@ -0,0 +1,13 @@
+package com.project.memmem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemmemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemmemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/memmem/MemmemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.memmem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemmemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
