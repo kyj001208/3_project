74646ddf73af8cb10331f58197dde0cdd16be1c8
ljw <jwoong1109@naver.com>
2024-08-30 17:39:06 +0900

    feat : imgaeupload

---
+++ src/main/java/com/project/memmem/domain/repository/group/GroupEntityRepository.java
@@ -0,0 +1,9 @@
+package com.project.memmem.domain.repository.group;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.memmem.domain.entity.GroupEntity;
+
+public interface GroupEntityRepository extends JpaRepository<GroupEntity, Long>{
+
+}
