f29ba0142927dfc8a101dd567ce5ad42007b7202
btj <taeju1119@gmail.com>
2024-09-03 11:48:46 +0900

    feat : fix

--- src/main/java/com/project/memmem/service/impl/IndexServiceProcess.java
+++ src/main/java/com/project/memmem/service/impl/IndexServiceProcess.java
@@ -1,6 +1,5 @@
 package com.project.memmem.service.impl;
 
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,29 +18,14 @@ import lombok.RequiredArgsConstructor;
 public class IndexServiceProcess implements IndexService {
 	
 	private final GroupEntityRepository groupRepository;
-	
-	
-	@Override
-	public void groupsList(Model model) {
-	    List<GroupEntity> groups = groupRepository.findAll();
+    private final String baseUrl = "https://s3.ap-northeast-2.amazonaws.com/jyj.img.host/";
 
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YY/MM/dd"); // 원하는 포맷
-
-	    List<GroupDTO> groupDTOs = groups.stream()
-	        .map(group -> {
-	            GroupDTO dto = new GroupDTO();
-	            dto.setId(group.getId());
-	            dto.setGroupName(group.getGroupName());
-	            dto.setGreeting(group.getGreeting());
-	            dto.setCreatedAt(group.getCreatedAt().format(formatter)); // 포맷된 날짜
-	            dto.setCategory(group.getCategory());
-	            return dto;
-	        })
-	        .collect(Collectors.toList());
-
-	    model.addAttribute("groups", groupDTOs);
-	}
-
-
-
+    @Override
+    public void groupsList(Model model) {
+        List<GroupEntity> groups = groupRepository.findAll();
+        List<GroupDTO> groupDTOs = groups.stream()
+            .map(group -> group.toGroupDTO(baseUrl))
+            .collect(Collectors.toList());
+        model.addAttribute("groups", groupDTOs);
+    }
 }
