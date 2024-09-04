17b5b5d7dfbe207db16942c27113f6213e341732
jyj <hikenny96@naver.com>
2024-09-04 17:35:14 +0900

    feat:try oauth2

--- src/main/java/com/project/memmem/security/MemmemUserDetails.java
+++ src/main/java/com/project/memmem/security/MemmemUserDetails.java
@@ -1,14 +1,16 @@
 package com.project.memmem.security;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.project.memmem.domain.entity.UserEntity;
 
 import lombok.Getter;
 @Getter
-public class MemmemUserDetails extends User {
+public class MemmemUserDetails extends User implements OAuth2User {
     private static final long serialVersionUID = 1L;
     private final String email;
     private final String name;
@@ -16,7 +18,8 @@ public class MemmemUserDetails extends User {
     //private final UserEntity userEntity;
     private final long userId;
     private final String number; // 수정된 부분
-    //private final Map<String, Object> attributes;
+    
+    private  Map<String, Object> attributes;
     
     public MemmemUserDetails(UserEntity entity) {
         super(entity.getEmail(), entity.getPassword(),
@@ -27,5 +30,10 @@ public class MemmemUserDetails extends User {
         this.userId = entity.getUserId();
         this.number = entity.getNumber(); // 수정된 부분
     }
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
     
 }
