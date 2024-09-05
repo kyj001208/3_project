42dc24c7734a463010fee66403d791177290b870
gon <lolaven@naver.com>
2024-09-05 11:10:25 +0900

    feat:mypage

--- src/main/java/com/project/memmem/controller/RecaptchaConfigController.java
+++ src/main/java/com/project/memmem/controller/RecaptchaConfigController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.memmem.service.impl.ReCaptchaService;
+import com.project.memmem.service.impl.RecaptchaV3Service;
 
 @RestController
 public class RecaptchaConfigController {
     
-    @Autowired
-    private ReCaptchaService reCaptchaService;
+	@Autowired
+    private RecaptchaV3Service reCaptchaService;
 
     @Value("${google.recaptcha.key.site}")
     private String recaptchaSiteKey;
@@ -32,7 +32,7 @@ public class RecaptchaConfigController {
     @PostMapping("/verify-recaptcha")
     public ResponseEntity<Map<String, Boolean>> verifyRecaptcha(@RequestBody Map<String, String> request) {
         String token = request.get("recaptchaToken");
-        boolean isValid = reCaptchaService.verifyReCaptcha(token);
+        boolean isValid = reCaptchaService.verifyRecaptcha(token);
         Map<String, Boolean> response = new HashMap<>();
         response.put("success", isValid);
         return ResponseEntity.ok(response);
